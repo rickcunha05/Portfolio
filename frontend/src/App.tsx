5d0a43916aedbb9071b6f211995ab2219341d6e7
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2023-05-26 14:29:40 -0300

    Settings theme in app.scss and install sass 👨‍💻

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import {About, Footer, Header, Skills, Testimonial, Work} from './container'
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { NavBar } from "./components";
+import "./App.scss";
 
 function App() {
-
   return (
-    <>
-    <h1>Hello Edoardaa!</h1>
-    </>
-  )
+    <div className="app">
+      <NavBar />
+      <About />
+      <Footer />
+      <Header />
+      <Skills />
+      <Testimonial />
+      <Work />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './NavBar.scss'
+
+export default function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}
