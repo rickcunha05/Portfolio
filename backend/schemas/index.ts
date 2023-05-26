1372f27cb5ed487d3a729d1c49132e2f988ec71e
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2023-05-26 00:26:55 -0300

    created schemas and finished backend!

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import testimonial from './testimonial'
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [
+  works,
+  testimonial,
+  brands,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+]
