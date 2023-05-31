1b6d7324344611cc8adaa9cfaa2dffb0ae46741f
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2023-05-31 16:42:41 -0300

    adjusts!

--- frontend/src/components/SocialMedia.tsx
+++ frontend/src/components/SocialMedia.tsx
@@ -1,16 +1,19 @@
-import { BsInstagram, BsTwitter } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { BsInstagram, BsGithub, BsFileEarmarkPdfFill } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
 export default function SocialMedia() {
   return (
     <div className="app__social">
       <div>
-        <BsTwitter />
-      </div>
-      <div>
-        <FaFacebookF />
+        <FaLinkedinIn />
       </div>
       <div>
         <BsInstagram />
+      </div>
+      <div>
+        <BsGithub />
+      </div>
+      <div>
+        <BsFileEarmarkPdfFill />
       </div>
     </div>
   );
