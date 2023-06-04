e400c587e75a7fa6a3331f7ab0075db8fcd7387b
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2023-06-04 00:11:20 -0300

    Adjustment
    Adjustment in the query of experiences to be aligned with the backend and now the experiences are listed in order

--- frontend/src/hooks/containers/useSkills.ts
+++ frontend/src/hooks/containers/useSkills.ts
@@ -8,12 +8,14 @@ export function useSkills() {
   const [skills, setSkills] = useState<Skills[]>([]);
 
   useEffect(() => {
-    const query = '*[_type == "experiences"]';
+    const query = '*[_type == "experiences"] | order(year asc)';
     const skillsQuery = '*[_type == "skills"]';
 
     client.fetch(query).then((data: Experiences[]) => {
+      data.sort((a, b) => a.year.localeCompare(b.year));
       setExperience(data);
     });
+
     client.fetch(skillsQuery).then((data: Skills[]) => {
       setSkills(data);
     });
