ffee134392011ad7d7948dad69a39b18a656ba53
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2023-06-05 11:21:19 -0300

    cleaning images.tsx import's

--- frontend/src/constants/images.tsx
+++ frontend/src/constants/images.tsx
@@ -25,15 +25,6 @@ import about04 from "../assets/about04.png";
 
 import profile from "../assets/profile.png";
 import circle from "../assets/circle.svg";
-import logo from "../assets/logo.png";
-
-import adidas from "../assets/adidas.png";
-import amazon from "../assets/amazon.png";
-import asus from "../assets/asus.png";
-import bolt from "../assets/bolt.png";
-import nb from "../assets/nb.png";
-import skype from "../assets/skype.png";
-import spotify from "../assets/spotify.png";
 
 export default {
   email,
